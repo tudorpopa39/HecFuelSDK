ed610fac26fcf1c02fea00291f1a63b60a64cf4c
Tudor Popa <tudor.popa@hpm.ro>
2019-07-15 12:46:25 +0300

    add framework

---
+++ HecFuelSDK.framework/Headers/hecfuel.h
@@ -0,0 +1,19 @@
+//
+//  hecfuel.h
+//  hecfuel
+//
+//  Created by Tudor Popa on 08/07/2019.
+//  Copyright © 2019 Tudor Popa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for hecfuel.
+FOUNDATION_EXPORT double hecfuelVersionNumber;
+
+//! Project version string for hecfuel.
+FOUNDATION_EXPORT const unsigned char hecfuelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hecfuel/PublicHeader.h>
+
+
